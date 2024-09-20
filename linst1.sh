dbce3bd50770ad2e5653c91dcdc8f3a2aea75c8e
braveua <braveua@gmail.com>
2024-09-20 04:27:29 +0300

    save

--- linst1.sh
+++ linst1.sh
@@ -4,8 +4,7 @@ sudo apt update -y
 sudo apt upgrade -y
 
 sudo apt install -y git vlc qbittorrent openssh-server
-#sudo apt install -y google-chrome-stable
-flatpak install com.google.Chrome
+sudo flatpak install -y com.google.Chrome
 
 sudo apt install -y far2l-gui
 
