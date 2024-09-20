baab4b8144f1570c36c1ee00b8f5156c9925cea6
braveua <braveua@gmail.com>
2024-09-20 03:56:59 +0300

    save

--- linst1.sh
+++ linst1.sh
@@ -1,11 +1,11 @@
 echo start
-sudo add-apt-repository ppa:far2l-team/ppa
+sudo add-apt-repository -y ppa:far2l-team/ppa
 sudo apt update -y
 sudo apt upgrade -y
 
 sudo apt install -y git vlc qbittorent openssh-server
-sudo apt-get install google-chrome-stable
+sudo apt install -y google-chrome-stable
 
-sudo apt install far2l-gui -y
+sudo apt install -y far2l-gui
 
 echo done
