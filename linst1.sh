38d57330c6b5ce66403b722591bfe9fb9fff9341
braveua <braveua@gmail.com>
2024-09-26 23:35:20 +0300

    save

--- linst1.sh
+++ linst1.sh
@@ -8,7 +8,10 @@ sudo flatpak install -y com.google.Chrome
 
 sudo apt install -y far2l-gui
 
+printf 'sshfs#brave@192.168.0.10:/media/3TB/share/ /home/brave/filesrv/share fuse defaults,idmap=user,port=22,allow_other,reconnect,_netdev,users,identityfile=/home/brave/.ssh/id_rsa 0 0\n' | sudo tee -a /etc/fstab
+printf 'sshfs#brave@192.168.0.10:/media/3TB/torrent/ /home/brave/filesrv/torrent fuse defaults,idmap=user,port=22,allow_other,reconnect,_netdev,users,identityfile=/home/brave/.ssh/id_rsa 0 0\n' | sudo tee -a /etc/fstab
 mkdir -p ~/filesrv/share
 mkdir -p ~/filesrv/torrent
+
 
 echo done
